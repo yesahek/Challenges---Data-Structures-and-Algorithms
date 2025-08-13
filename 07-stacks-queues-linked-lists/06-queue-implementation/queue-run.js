c66740012e5b2c31693e6d75fe2a77967c5f59a4
yesahek <yesahek@gmail.com>
2025-08-13 23:29:34 +0300

    queue-implementaion

---
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue-run.js
@@ -0,0 +1,21 @@
+const Queue = require('./queue');
+
+const queue = new Queue();
+queue.enqueue('a');
+queue.enqueue('b');
+queue.enqueue('c');
+//console.log(queue);
+queue.dequeue();
+console.log(queue.peek());
+
+
+// const queue = new Queue();
+// console.log(queue.isEmpty());
+// console.log(queue.isFull());
+// console.log(queue.enqueue(1));
+// console.log(queue.enqueue(2));
+// console.log(queue.enqueue(3));
+// console.log(queue.peek());
+// console.log(queue.dequeue());
+// console.log(queue.peek());
+// console.log(queue.getLength());
